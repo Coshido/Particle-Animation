5f89e95561d6ef78f65a4960375d69703e2a1945
Coshido <coshido@gmail.com>
2023-01-24 13:18:18 +0100

    add footer

--- javascript.js
+++ javascript.js
@@ -128,3 +128,17 @@ image.addEventListener("load", function () {
   }
   animate();
 });
+
+function createFooter() {
+  let footer = document.createElement("footer");
+  let footerLink = document.createElement("a");
+  footerLink.classList.add("footer-link");
+  footerLink.href = "https://github.com/Coshido?tab=repositories";
+  footerLink.target = "_blank";
+  footerLink.innerHTML = `Copyrights © 2022 Coshido  <i class="fab fa-github"></i>`;
+  footer.appendChild(footerLink);
+
+  return footer;
+}
+const footer = createFooter();
+document.querySelector("body").appendChild(footer);
